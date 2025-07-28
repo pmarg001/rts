776716a9ade173f5fa354f2b23c915177a8dd0ee
Margi <margipatel01@outlook.com>
2025-07-27 21:03:34 -0400

    End of section 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import ReactDOM from "react-dom/client";
+// import Parent from './props/Parent';
+// import GuestList from './state/GuestList';
+// import UsersList from './state/UsersList';
+// import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
+
 
 const el = document.getElementById("root");
+// document.querySelector('#root');
 
 const root = ReactDOM.createRoot(el!);
 
@@ -8,6 +15,7 @@ const App = () => {
   return (
     <div>
       <h1>Hi there!</h1>
+      <UserSearch />
     </div>
   );
 };
